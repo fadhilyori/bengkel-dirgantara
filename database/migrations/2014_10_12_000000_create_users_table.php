5502f8cffa363025eed985af0d7c180646b94c62
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-12-13 10:13:26 +0700

    fix user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,19 +14,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('pegawai', function (Blueprint $table) {
+            // $table->increments('id');
+            // $table->string('name', 50);
+            // $table->string('alamat', 50);
+            // $table->string('no_telp', 191);
+            // $table->date('tanggal_masuk');
+            // $table->date('tanggal_keluar')->nullable();
+            // $table->string('email', 50)->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password', 191);
+            // $table->unsignedInteger('role_id');
+            // $table->unsignedInteger('manajer_id')->nullable();
+            // $table->unsignedInteger('branch_id');
+            // $table->unsignedInteger('status_id');
+            // $table->rememberToken();
+            // $table->timestamps();
             $table->increments('id');
-            $table->string('name', 50);
-            $table->string('alamat', 50);
-            $table->string('no_telp', 191);
-            $table->date('tanggal_masuk');
-            $table->date('tanggal_keluar')->nullable();
-            $table->string('email', 50)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 191);
-            $table->unsignedInteger('role_id');
-            $table->unsignedInteger('manajer_id')->nullable();
-            $table->unsignedInteger('branch_id');
-            $table->unsignedInteger('status_id');
+            $table->string('name');
+            $table->string('email', 191)->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
