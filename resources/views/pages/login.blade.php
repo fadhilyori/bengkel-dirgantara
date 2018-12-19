593427f511a08f8de11a2cf98283d569f4f8e83d
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-12-19 15:16:27 +0700

    merubah tampilan login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -27,10 +27,10 @@
 <body class="app flex-row align-items-center">
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-5">
             <div class="card-group">
                 <div class="card p-4">
-                    <div class="card-body">
+                    <div class="card-body" style="background-image: url('{{ asset('/img/logoweb.png') }}');">
                         <h1>Login</h1>
                         <p class="text-muted">Sign In to your account</p>
                         <form method="POST" action="{{ route('login') }}">
@@ -55,16 +55,6 @@
                                 </div>
                             </div>
                         </form>
-                    </div>
-                </div>
-                <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-                    <div class="card-body text-center">
-                        <div>
-                            <h2>Sign up</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua.</p>
-                            <a href="{{ route('register') }}" class="btn btn-primary active mt-3">Register Now!</a>
-                        </div>
                     </div>
                 </div>
             </div>
