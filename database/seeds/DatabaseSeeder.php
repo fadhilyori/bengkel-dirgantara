202bb03ec43a3792aa240c979dcba30c1967c815
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-12-19 15:15:01 +0700

    add JobSeeder in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TypeSeeder::class);
         $this->call(KendaraanSeeder::class);
         $this->call(PartSeeder::class);
+        $this->call(JobSeeder::class);
     }
 }
